10d39f7a5dc1e5467fef7ef0a19ef15ba2bcb9bc
abdallahanwar899@gmail.com <83489211+3bdallah99@users.noreply.github.com>
2025-12-08 23:59:58 +0200

    feat: Add footer component, internationalization files, and initial page layouts.

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule, TranslateModule],
+  imports: [CommonModule, RouterLink, TranslateModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
   currentYear = new Date().getFullYear();
 }
+
